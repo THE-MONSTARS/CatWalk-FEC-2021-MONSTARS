958d23c672d77e2b4d35fb1d8dad082bd2cb1b1c
Erik Oh <eoh555@gmail.com>
2021-06-04 13:57:33 -0700

    Revised 'OverView' to 'UnderView'

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const OverView = () => (
+const UnderView = () => (
   <h1>This is the overview, goodbye.</h1>
 )
 
-export default OverView;
+export default UnderView;
