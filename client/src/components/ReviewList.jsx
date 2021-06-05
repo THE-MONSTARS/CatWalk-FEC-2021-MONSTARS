d005b77b70b396182d5a84deb822c13b5603ba02
raphspies <raphspies@gmail.com>
2021-06-05 12:18:19 -0700

    added temporary axios GET call to atelier's /products/ endpoint to get test data

--- client/src/components/ReviewList.jsx
+++ client/src/components/ReviewList.jsx
@@ -1,16 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import credentials from '../../../config.js'
 
 class ReviewList extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      reviews: []
+      reviews: [],
     }
+    this.updateReviews = this.updateReviews.bind(this);
   }
+
+  componentDidMount() {
+    axios({
+      method: 'GET',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/',
+      headers: { 'Authorization': credentials.TOKEN },
+    })
+    .then(res => {
+      console.log(res.data)
+      // this.updateReviews(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  updateReviews(data) {
+    this.setState({
+      reviews: data,
+    })
+
+  }
+
   render() {
     return (
       <div>ReviewList</div>
     )
   }
 }
+
+// function ReviewList() {
+//   const [reviews setReviews] = useState([]);
+
+//   return (
+//     <div></div>
+//   )
+// }
+
+
+
+
+
+
+
+
+
+
 export default ReviewList
