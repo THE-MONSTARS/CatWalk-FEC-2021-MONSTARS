83fc497c1aa1a168f709ad294fb9fc22ae924ec3
raphspies <raphspies@gmail.com>
2021-06-05 16:30:35 -0700

    Added getReviews function which appropriately queries atelier API

--- client/src/components/ReviewList.jsx
+++ client/src/components/ReviewList.jsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import credentials from '../../../config.js'
+// import API from '../../../server/router.js'
 
 class ReviewList extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      product: "",
       reviews: [],
     }
+    this.getProductList = this.getProductList.bind(this);
+    this.getReviews = this.getReviews.bind(this);
     this.updateReviews = this.updateReviews.bind(this);
   }
 
   componentDidMount() {
+    this.getProductList()
+    this.getReviews(16057)
+  }
+
+  getProductList() {
     axios({
       method: 'GET',
       url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/',
@@ -23,6 +32,18 @@ class ReviewList extends React.Component {
     })
     .catch(err => {
       console.log(err)
+    })
+  }
+
+  getReviews(product_id) {
+    const params = new URLSearchParams({ sort: "newest", product_id: product_id, })
+    axios({
+      method: 'GET',
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/reviews?${params}`,
+      headers: { 'Authorization': credentials.TOKEN },
+    })
+    .then(res => {
+      console.log(res.data.results)
     })
   }
 
