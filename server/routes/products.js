4a072a64774d223e1c491b579844672fd0a2e7f1
Nick Kim <nicholaskim101@yahoo.com>
2021-06-08 12:16:01 -0700

    'Started get styles route and updated getproduct by Id with correct param'

--- server/routes/products.js
+++ server/routes/products.js
@@ -19,7 +19,7 @@ products
 products
   .route('/:product_id')
   .get((req, res) => {
-    return API.getProductById(req.params.id)
+    return API.getProductById(req.params.product_id)
     .then(data => {
       res.status(200).send(data)
     })
@@ -31,16 +31,22 @@ products
 // return all styles available for the given product
 products
   .route('/:product_id/styles')
-  .get()
+  .get((req, res) => {
+    console.log('This is the req obj...', req.params.product_id)
+    return API.getStyleById(req.params.product_id)
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(err => {
+      res.status(400).send('error retrieving styles by id: ', err)
+    })
+  })
 
   // return the id's of products related to the product specified
 products
   .route('/:product_id/related')
-  .get(((req, res) => {
-    getRelated((err, data) => {
+  .get()
 
-    })
-  }))
 
   module.exports = products;
 
