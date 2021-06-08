a86b15bac1ab2bb6760d0480ac08a111513c5787
raphspies <raphspies@gmail.com>
2021-06-07 17:59:28 -0700

    Added router endpoints for reviews/:productId, products & :/product_id, and atelier axios requests inside API for each endpoint to call

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const reviews = express.Router();
+const API = require('../../apiHelpers/atelier')
 
 reviews
-  .route('/')
+  .route('/:product_id/:sort_order?')
   .get((req, res) => {
-    res.status(200).send('hello from reviews!')
+    var product_id = req.params.product_id;
+    console.log(req.params)
+    var sort_order = 'newest';
+    if (req.params.sort_order) {
+      sort_order = req.params.sort_order
+    }
+    API.getReviews(product_id, sort_order, (err, response) => {
+      if (err) {
+        console.log(err)
+        res.status(err)
+      } else {
+        res.status(200).send(response)
+      }
+    })
   })
 
   module.exports = reviews;
