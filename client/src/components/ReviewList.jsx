891d60ad79191be7c8dca341656ea4b2f0d8898d
raphspies <raphspies@gmail.com>
2021-06-08 11:02:09 -0700

    added punctuation, removed unused comments

--- client/src/components/ReviewList.jsx
+++ client/src/components/ReviewList.jsx
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import credentials from '../../../config.js'
-// import API from '../../../server/router.js'
 import Review from './Review.jsx'
 const url = 'http://localhost:3000'
 
 function ReviewList() {
   const [ isLoading, setIsLoading ] = useState(false);
   const [ reviews, setReviews ] = useState([]);
-  const [ productId, setProductId ] = useState(16057)
+  const [ productId, setProductId ] = useState(16057);
 
-  //update reviews
   useEffect(() => {
     function getReviews() {
       setIsLoading(true)
@@ -29,7 +27,7 @@ function ReviewList() {
     }
     getReviews();
   }, [])
-  // return //cleanup
+   //cleanup?
 
   return (
     isLoading ?
@@ -41,79 +39,4 @@ function ReviewList() {
   )
 }
 
-// const getReviews = (product_id) => {
-//   const params = new URLSearchParams({ sort: "newest", product_id: product_id, })
-//   await axios({
-//     method: 'GET',
-//     url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/reviews?${params}`,
-//     headers: { 'Authorization': credentials.TOKEN },
-//   })
-//   .then(res => {
-//     setReviews(res.data.results)
-//     console.log(res.data.results);
-//   })
-// }
-
-// class ReviewList extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       product: "",
-//       reviews: [],
-//     }
-//     this.getProductList = this.getProductList.bind(this);
-//     this.getReviews = this.getReviews.bind(this);
-//     this.updateState = this.updateState.bind(this);
-//   }
-
-//   componentDidMount() {
-//     this.getProductList()
-//     this.getReviews(16057)
-//   }
-
-//   getProductList() {
-//     axios({
-//       method: 'GET',
-//       url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/',
-//       headers: { 'Authorization': credentials.TOKEN },
-//     })
-//     .then(res => {
-//       console.log(res.data)
-//       // this.updateReviews(data)
-//     })
-//     .catch(err => {
-//       console.log(err)
-//     })
-//   }
-
-//   updateState(state, value) {
-//     this.setState({
-//       [state]: value,
-//     })
-
-//   }
-
-//   render() {
-//     var mappedReviews = this.state.reviews.map((review, index) => (
-//         <Review summary={review.summary} body={review.body} date={review.date} key={index} reviewer_name={review.reviewer_name} rating={review.rating}/>
-//       )
-//     )
-
-//     return (
-//       <div>
-//         Reviews:
-//         {mappedReviews}
-//       </div>
-//     )
-//   }
-// }
-
-// function ReviewList() {
-//   const [reviews setReviews] = useState([]);
-
-//   return (
-//     <div></div>
-//   )
-// }
-
-export default ReviewList
+export default ReviewList;
