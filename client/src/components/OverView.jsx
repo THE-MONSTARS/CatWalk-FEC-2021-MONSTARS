194172a65a1f4bf20116f30448b4441192b706e9
Erik Oh <eoh555@gmail.com>
2021-06-09 15:40:08 -0700

    Working style selector with basic basic styling. Incorporated styled components. image gallery reflects style

--- client/src/components/OverView.jsx
+++ client/src/components/OverView.jsx
@@ -4,23 +4,40 @@ import ProductInformation from './OverView/ProductInformation.jsx';
 import StyleSelector from './OverView/StyleSelector.jsx';
 import AddToCart from './OverView/AddToCart.jsx';
 import ProductDescription from './OverView/ProductDescription.jsx';
+import styled from 'styled-components';
+
+
+const OverViewContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 450px;
+`
+
+const ProductInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
 const OverView = ({currentProduct, styles, getStyles}) => {
-  const [ currentStyle, setCurrentStyle ] = useState({})
+  const [ currentStyle, setCurrentStyle ] = useState(styles[0])
 
   const selectStyle = (id) => {
     const current = styles.find(style => style.style_id === parseInt(id))
+    console.log(id)
     setCurrentStyle(current)
   }
 
   return (
     <div>
-      <ImageGallery />
-      <div className="container-column product-info">
-        <ProductInformation />
-        <StyleSelector styles={styles} selectStyle={selectStyle}/>
-        <AddToCart />
-      </div>
+      <OverViewContainer>
+        <ImageGallery currentStyle={currentStyle}/>
+        <ProductInfoContainer>
+          <ProductInformation />
+          <StyleSelector styles={styles} selectStyle={selectStyle}/>
+          <AddToCart />
+        </ProductInfoContainer>
+      </OverViewContainer>
+
       <ProductDescription />
     </div>
   )
