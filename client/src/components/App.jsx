caf45c26a0c6615d971147b6ee92cdfb42f4be89
Erik Oh <eoh555@gmail.com>
2021-06-08 19:18:17 -0700

    Refactor app to functional component. Currently able to fetch data for products, styles, and set current product. need to determine proper data to be passed down to children

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import OverView from './OverView.jsx';
 import ReviewList from './ReviewList.jsx';
 import RelatedContainer from './RelatedContainer.jsx';
+import axios from 'axios';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      something: ''
+
+const App = () => {
+  const [ products, setProducts ] = useState([])
+  const [ currentProduct, setCurrentProduct ] = useState()
+  const [ styles, setStyles ] = useState([])
+
+
+  const fetchProductAndId = async () => {
+    try {
+      const fetchedProducts = await axios.get('/products')
+      const productsData = fetchedProducts.data;
+      setProducts(productsData);
+      setCurrentProduct(productsData[0]);
+
+      const productId = productsData[0].id;
+      const fetchedStyles = await axios.get(`/products/${productId}/styles`);
+      const stylesData = fetchedStyles.data.results;
+      setStyles(stylesData);
+
+    } catch (err) {
+      console.log('error fetching product & styles data: ', err);
     }
   }
 
-  componentDidMount() {
 
-  }
+  useEffect(() => {
+    fetchProductAndId()
 
-  componentDidRender() {
+  }, []);
 
-  }
 
-  render() {
     return (
       <div>
-        Hello from React!
+        <OverView />
         <RelatedContainer />
         <ReviewList />
       </div>
-
     );
-  }
+
 }
 
 export default App;
