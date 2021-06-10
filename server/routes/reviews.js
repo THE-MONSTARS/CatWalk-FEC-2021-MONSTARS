9fdb0b2511d9d08d3438d4cdbd0eec188b40f387
Nick Kim <nicholaskim101@yahoo.com>
2021-06-10 11:35:28 -0700

    'Added Api Helper for meta reviews and Route for meta reviews'

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -3,6 +3,20 @@ const reviews = express.Router();
 const API = require('../../apiHelpers/atelier')
 
 reviews
+  .route('/:product_id/meta')
+  .get((req, res) => {
+    let product_id = req.params.product_id;
+    console.log(product_id)
+    return API.getReviewMeta(product_id)
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(err => {
+      res.status(400).send('error retrieving reviews meta: ', err)
+    })
+  })
+
+reviews
   .route('/:product_id/:sort_order?')
   .get((req, res) => {
     let product_id = req.params.product_id;
@@ -10,6 +24,7 @@ reviews
     if (req.params.sort_order) {
       sort_order = req.params.sort_order
     }
+    console.log('oh yea')
     return API.getReviews(product_id, sort_order)
     .then(data => {
       res.status(200).send(data)
