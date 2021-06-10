17b40db2836e819a0224006638c4328187f00fd2
Nick Kim <nicholaskim101@yahoo.com>
2021-06-10 10:52:11 -0700

    'Refactored with App functions for product info and style into RelatedContainer component'

--- client/src/components/RelatedContainer.jsx
+++ client/src/components/RelatedContainer.jsx
@@ -3,14 +3,74 @@ import React, {useState, useEffect} from 'react';
 import Card from './Card.jsx';
 import axios from 'axios';
 
-export default function RelatedContainer () {
+export default function RelatedContainer ({id, getOneProduct, getStyles}) {
+
   //Need to be able to get an array of all apporiate data
   //could look into like a promise all to grab both style and product data then create an array of objects with just the
   //related data needed. {category,name,img,star,def price, sale price }
 
   //style data need to search styles find default? to be true and use that data...
   const [productsInfo, setProductsInfo] = useState([1,2,3])//DummyData set to 3 indexes
-  const dummyId = 1;
+  const [loading, setLoading] = useState(true)
+  const dummyId = 16059;
+
+
+    function getRelatedProducts(productId) {
+      const fetchedRelatedProducts = axios.get(`/products/${productId}/related`);
+      return fetchedRelatedProducts;
+  }
+
+    async function getProductDetails(productId) {
+      const fetchedProduct = await axios.get(`/products/${productId}`);
+      return fetchedProduct.data;
+
+  }
+
+
+  async function getProductStyle(productId) {
+    const rawData = await axios.get(`/products/${productId}/styles`)
+    const style = rawData.data.results.find(entry =>
+        entry['default?'] === true
+      )
+    return style
+    }
+
+
+  const startup = async () => {
+    let relatedProductIds = await getRelatedProducts(id) // assuming this is async function
+
+    const relatedProducts = [];
+    console.log('Array of Product Ids', relatedProductIds.data)
+    for (let productId of relatedProductIds.data) {
+
+      const promises = [getOneProduct(productId), getStyles(productId)]
+
+      let [ productDetail, productStyles ] = await Promise.all(promises)
+
+      let currentProductData = {};
+
+      currentProductData.id = productDetail.id;
+      currentProductData.category = productDetail.category;
+      currentProductData.name = productDetail.name;
+
+      let defaultStyle = productStyles.find(entry => entry['default?'] === true || productStyles[0])
+
+      currentProductData.sale_price = defaultStyle.sale_price;
+      currentProductData.original_price = defaultStyle.original_price;
+      currentProductData.image = defaultStyle.photos[0].url
+
+
+      relatedProducts.push(currentProductData);
+    }
+    setProductsInfo(relatedProducts)
+    setLoading(false)
+  }
+
+
+  useEffect(()=> {
+    startup()
+  }, [])
+
 
 
   return (
@@ -19,7 +79,17 @@ export default function RelatedContainer () {
       {productsInfo.map((entry, index) => (
         <Card key={index} />
       ))}
+      {/* {loading ? <p>Loading...</p> : <p>Not loading! {productsInfo[0].name}</p>} */}
     </div>
   )
 
 }
+
+
+
+
+
+
+
+
+
