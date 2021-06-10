b7f53d813464f0c2d0046f7add22a8877c5fe096
Erik Oh <eoh555@gmail.com>
2021-06-10 13:40:17 -0700

    implement cart size and cart quantity to play nice together

--- client/src/components/OverView/AddToCart.jsx
+++ client/src/components/OverView/AddToCart.jsx
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 
 
 
 
 const AddToCart = ({currentStyle}) => {
-  const sizes = currentStyle.skus;
+  const [ sizeId, setSizeId ] = useState('')
+  const [ itemStock, setItemStock ] = useState(10)
+  const [ quantity, setQuantity ] = useState(0)
+
+  // API data gives an object of objects. Converting to Array of objects
+  const sizes = Object.entries(currentStyle.skus).map(entry => {
+    return { id: entry[0], quantity: entry[1].quantity, size: entry[1].size }
+  })
+
+  const getStock = () => {
+    for (const size of sizes) {
+      if (size.id === sizeId) { return size.quantity }
+    }
+  }
+
+  const createSelectItems = () => {
+    const items = [];
+    for (let i = 1; i <= (itemStock > 15 ? 15 : itemStock); i++) {
+      items.push(i)
+    }
+    return items
+  }
+
+  useEffect(() => {
+    let stock = getStock();
+    setItemStock(stock)
+  }, [sizeId])
+
+  const quantityDropDown = createSelectItems();
+
   return (
     <div className="add-to-cart">
-      <select className="select-size">
+      <select className="select-size" onChange={e => setSizeId(e.target.selectedOptions[0].attributes['data-id'].value)}>
+        <option value={null}>-</option>
         {sizes.map(size => (
-          <option value={size.size}>{size.size}</option>
+          <option key={size.id} data-id={size.id} value={size.size}>{size.size}</option>
         ))}
-        <option value="small">small</option>
-        <option value="medium">medium</option>
-        <option value="large">large</option>
       </select>
 
-      <select className="select-quantity">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
+      <select className="select-itemStock" onChange={e => setQuantity(e.target.value)}>
+        {quantityDropDown.map(itemStock => (
+          <option key={itemStock} value={itemStock}>{itemStock}</option>
+        ))}
       </select>
 
       <button className="add-to-bag" type="button">ADD TO BAG</button>
