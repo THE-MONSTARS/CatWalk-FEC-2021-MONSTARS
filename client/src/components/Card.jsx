d4afff10a7b44a136e31a03834a5cd9a6b69ba8b
Nick Kim <nicholaskim101@yahoo.com>
2021-06-11 15:16:12 -0700

    'Made some code DRY in Related Container and added setProduct functionality in Card Componenet'

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import StarRating from './StarRating.jsx';
+import RelatedAction from './RelatedAction.jsx';
 
 const StyleCard = styled.div`
 display: flex;
@@ -11,19 +12,19 @@ border: 1px solid;
 padding: 10px;
 `
 
-export default function Card (props) {
+export default function Card ({product, setCurrentProduct}) {
 
   //Will need url and pricing from styles another get request
   //All Get functions will prob be on a component level higher...
 
   return (
-    <StyleCard>
-      <p>Some overlay action Component</p>
-      <img src={props.img}></img>
-      <h4>{props.category}</h4>
-      <h2>{props.name}</h2>
-      <h3>{props.salePrice ? props.salePrice : props.orginalPrice}</h3>
-      <StarRating rating={props.rating}/>
+    <StyleCard id={product.id} onClick={() => setCurrentProduct(product)}>
+      <RelatedAction />
+      <img src={product.image}></img>
+      <h4>{product.category}</h4>
+      <h2>{product.name}</h2>
+      <h3>{product.salePrice ? product.sale_price : product.original_price}</h3>
+      <StarRating rating={product.rating}/>
     </StyleCard>
   )
 
