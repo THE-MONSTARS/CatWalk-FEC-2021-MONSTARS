768d73a29850439908252ac4495c2882d271fb6d
Erik Oh <eoh555@gmail.com>
2021-06-17 17:09:38 -0700

    Small change to features view in overview

--- client/src/components/OverView/ProductDescription.jsx
+++ client/src/components/OverView/ProductDescription.jsx
@@ -35,8 +35,16 @@ const FeatureContainer = styled.div`
 
 const Feature = styled.li`
   font-family: 'Roboto';
-`
+  font-size: 12px;
+  margin: 10px 0;
 
+`
+const FeatureBold = styled.b`
+  font-family: 'Roboto';
+  font-size: 14px;
+  border-bottom: 1px dotted #343a40;
+  padding 2px;
+`
 
 
 const ProductDescription = ({currentProduct}) => {
@@ -55,7 +63,7 @@ const ProductDescription = ({currentProduct}) => {
       <FeatureContainer>
         <ul>
           {features && features.map((feature, idx) => (
-            <Feature key={idx}>{feature.feature}: {feature.value}</Feature>
+            <Feature key={idx}><FeatureBold>{feature.feature}</FeatureBold> -        {feature.value}</Feature>
           ))}
         </ul>
       </FeatureContainer>
